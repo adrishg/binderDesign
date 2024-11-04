c261ed672bd68f20951a8de8bed03552097abe6c
adrishg <ahgonzalez@ucdavis.edu>
2024-11-04 15:10:25 -0800

    step one flows ok

--- 0-binder_de_novo.sh
+++ 0-binder_de_novo.sh
@@ -31,29 +31,37 @@ mkdir -p "$project_path"/{1-BackboneDesign,1.5-FilteringBackbones,2-SequenceDesi
 # --wait falg is neccessary in order to let the job end until starts the next step
 #Generate 100 backbones
 
-sbatch --wait 1-run_backboneDesign.sh -o "$project_name" [-i INPUT_PDB] [-g CONTIGMAP] -n 100
+#Usage:
+#sbatch --wait 1-run_backboneDesign.sh -o "$project_name" [-i INPUT_PDB] [-g CONTIGMAP] -n 100
+#Example Cav2.2_AID
+sbatch --wait 1-run_backboneDesign.sh \
+    "/share/yarovlab/ahgz/Binders-Review/Cav22_AID_site/Test-1/output100/" \
+    "/share/yarovlab/ahgz/Binders-Review/Cav22_AID_site/Test-1/inputs/7miy_truncated.pdb" \
+    "[A332-406/0 A464-786/0 60-100]" \
+    "[A381,A384,A385,A388,A389,A391,A392]" \
+    100
 
 # Step 1.5: Filtering Backbones
 # In this case waits automatically
 # Filter 1: in this case to make the C and N terminus to be in the same side and far from the interacting region between A and B, padding and tolerance might need fine tuning
 
-python /share/yarovlab/ahgz/scripts/binderDesign/1_5_filter_NCterminus.py -d /path/to/pdb_directory -p 10.0 -t 60.0
+#python /share/yarovlab/ahgz/scripts/binderDesign/1_5_filter_NCterminus.py -d /path/to/pdb_directory -p 10.0 -t 60.0
 
 # More filters TBA
 
 # Step 2: Sequence Design with Soluble MPNN
 #Generate sequences with Soluble MPNN chain A is always binder in RFDiffusion
 
-sbatch --wait /share/yarovlab/ahgz/scripts/binderDesign/2-sequenceDesign_solubleMPNN_de_novo.sh -f "$project_path/1.5-FilteringBackbones/output/" -o "$project_path/2-SequenceDesign" -c 'A'
+#sbatch --wait /share/yarovlab/ahgz/scripts/binderDesign/2-sequenceDesign_solubleMPNN_de_novo.sh -f "$project_path/1.5-FilteringBackbones/output/" -o "$project_path/2-SequenceDesign" -c 'A'
 
 # Step 3: Foldability Test of binder only
 # Part 1: actually running AF2 for all sequences, this is the slowest step in my experience
-sbatch --wait /share/yarovlab/ahgz/scripts/binderDesign/3-run_FoldabilityTest.sh -s "$project_path/2-SequenceDesign" -o "$project_path/3-FoldabilityTest/output/"
+#sbatch --wait /share/yarovlab/ahgz/scripts/binderDesign/3-run_FoldabilityTest.sh -s "$project_path/2-SequenceDesign" -o "$project_path/3-FoldabilityTest/output/"
 
 # Part 2: Foldability Test Filtering and Plotting
 # Run final Python script to generate plots and summary
 #Plot the results by pLDDT and RMSD, returns summary with list of design squences that have pLDDT > 95 and RMSD < 1.5 A and scatterplot png
 
-python /share/yarovlab/ahgz/scripts/binderDesign/3-FoldabilityTest_plot_filter.py --folder_of_folders "$project_path/3-FoldabilityTest/output/" --reference_folder "$project_path/1-BackboneDesign/output" --output_csv "output.csv" --filtered_output_csv "filtered_output.csv" --summary_file "summary_foldabilityTest.txt" --plot_file "output_directory/pldds_vs_rmsd_plot.png" --plddt_threshold 95 --rmsd_threshold 2
+#python /share/yarovlab/ahgz/scripts/binderDesign/3-FoldabilityTest_plot_filter.py --folder_of_folders "$project_path/3-FoldabilityTest/output/" --reference_folder "$project_path/1-BackboneDesign/output" --output_csv "output.csv" --filtered_output_csv "filtered_output.csv" --summary_file "summary_foldabilityTest.txt" --plot_file "output_directory/pldds_vs_rmsd_plot.png" --plddt_threshold 95 --rmsd_threshold 2
 
 #Step 4: AF multimer and Docking(?) step
