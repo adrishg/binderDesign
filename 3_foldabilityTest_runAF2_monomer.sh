152b7adf44679f3aa37610a00afd42331caf7682
adrishg <ahgonzalez@ucdavis.edu>
2025-04-19 14:23:58 -0600

    corrections monomer version ignore all G

--- 3_foldabilityTest_runAF2_monomer.sh
+++ 3_foldabilityTest_runAF2_monomer.sh
@@ -52,22 +52,28 @@ for fasta_file in "$SEQ_FOLDER"/*.fa; do
     current_seq=""
     current_alias=""
     original_header=""
+    header_written=0
 
     while IFS= read -r line || [[ -n "$line" ]]; do
         if [[ "$line" =~ ^\> ]]; then
-            if [[ -n "$current_seq" && -n "$current_alias" ]]; then
+            # Write previous sequence if valid
+            if [[ -n "$current_seq" && $header_written -eq 1 ]]; then
                 truncated_seq="${current_seq%%:*}"
                 if [[ "$truncated_seq" =~ ^G+$ ]]; then
                     echo "Skipping all-Glycine sequence for $current_alias"
+                    sed -i '$d' "$processed_fasta"  # remove previous header line
                     ((seq_counter--))  # undo alias increment
                 else
                     echo "$truncated_seq" >> "$processed_fasta"
                     echo "$current_alias,\"$original_header\",$truncated_seq" >> "$reference_csv"
                 fi
             fi
+
+            # Prepare for new sequence
             original_header=${line#>}
             current_alias="${ALIAS_PREFIX}_${base_name}_${seq_counter}"
             echo ">$current_alias" >> "$processed_fasta"
+            header_written=1
             current_seq=""
             ((seq_counter++))
         else
@@ -76,10 +82,11 @@ for fasta_file in "$SEQ_FOLDER"/*.fa; do
     done < "$fasta_file"
 
     # Final sequence
-    if [[ -n "$current_seq" && -n "$current_alias" ]]; then
+    if [[ -n "$current_seq" && $header_written -eq 1 ]]; then
         truncated_seq="${current_seq%%:*}"
         if [[ "$truncated_seq" =~ ^G+$ ]]; then
             echo "Skipping all-Glycine sequence for $current_alias"
+            sed -i '$d' "$processed_fasta"  # remove header line
         else
             echo "$truncated_seq" >> "$processed_fasta"
             echo "$current_alias,\"$original_header\",$truncated_seq" >> "$reference_csv"
