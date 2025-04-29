0ad00cfe095c4cb76f6763802131387cd38dd2f4
adrishg <ahgonzalez@ucdavis.edu>
2025-04-29 00:49:47 -0700

    lower even more time Foldability to be able to run before mantainance change later back up

--- 3_foldabilityTest_runAF2_monomer.sh
+++ 3_foldabilityTest_runAF2_monomer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -p gpu-vladimir
 #SBATCH --gres=gpu:1
-#SBATCH -t 7-12:00:00
+#SBATCH -t 5-12:00:00
 #SBATCH --job-name=foldabilityTest
 #SBATCH --mem=125G
 #SBATCH --mail-user=ahgonzalez@ucdavis.edu
