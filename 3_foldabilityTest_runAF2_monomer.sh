fcc50d63b8f38ffbe89494aac3faaf9d483b48fb
adrishg <ahgonzalez@ucdavis.edu>
2025-07-25 00:09:24 -0700

    one seed in monomer to save time normally 2

--- 3_foldabilityTest_runAF2_monomer.sh
+++ 3_foldabilityTest_runAF2_monomer.sh
@@ -100,6 +100,6 @@ for fasta_file in "$SEQ_FOLDER"/*.fa; do
     parallel -j $NUM_PARALLEL CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES colabfold_batch \
         --msa-mode single_sequence \
         --num-recycle 3 \
-        --num-seed 2 \
+        --num-seed 1 \
         {} "$output_dir" ::: "$split_dir"/batch*.fa
 done
